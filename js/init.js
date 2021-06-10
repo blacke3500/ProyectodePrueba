55491ecf687a23fe3782d4f7eba3fefe0dd4936d
blacke3500 <coollopez2@gmail.com>
2021-06-10 18:37:18 -0500

    Cambio en la llave

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@
  * quedar igual que en tu
  * servidor. */
 // @ts-ignore
-  var firebaseConfig = {
+    firebase.initializeApp({
     apiKey: "AIzaSyDe9KdQGd0kxRyeNqOf0WcAxxto1qBwJOk",
     authDomain: "proyecto-final-711df.firebaseapp.com",
     projectId: "proyecto-final-711df",
@@ -15,4 +15,4 @@
     messagingSenderId: "864319190534",
     appId: "1:864319190534:web:c389ee053ff9097bdef587",
     measurementId: "G-6GDFRYMXMM"
-  };
+  });
